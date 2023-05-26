c18676a19d3f0a640ca89a82ce3a5ad4cf6a127b
DESKTOP-B6U7AQJ\Andra <gheorghe.andra.r2x@student.ucv.ro>
2023-05-26 23:05:28 +0300

    Functional door with keypad

--- Andra's maze/Assets/Scripts/KeyPad.cs
+++ Andra's maze/Assets/Scripts/KeyPad.cs
@@ -7,12 +7,29 @@ using UnityEngine.UI;
 public class KeyPad : MonoBehaviour
 {
     [SerializeField] private TMP_Text ans;
+    private GameObject secondDoor;
+    private string correctPincode = "1212";
+    private bool doorOpened = false;
 
+    public void Start()
+    {
+        secondDoor = GameObject.FindWithTag("SecondDoor");
+    }
     public void Number(int number)
     {
         if (ans.text.Length < 4)
         {
             ans.text += number.ToString();
+        }
+    }
+
+    public void Confirm()
+    {
+        if (ans.text == correctPincode && !doorOpened)
+        {
+            secondDoor.transform.position = new Vector3(294.78f, 0.87f, -334.1f);
+            secondDoor.transform.Rotate(0, -100f, 0.0f, Space.Self);
+            doorOpened = true;
         }
     }
 }
